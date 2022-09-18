b275b5be1f97bdb2a6b7b4d7cb6d59994003af9a
Selftishz <art2sys048@gmail.com>
2022-09-18 23:47:19 +0300

    Init

---
+++ src/Mirea/Author.java
@@ -0,0 +1,37 @@
+package Mirea;
+
+public class Author {
+
+    private String name;
+    private String email;
+    private char gender;
+
+    public Author(String name, String email, char gender) {
+        this.email = email;
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return " name: " + name +
+                " email: " + email +
+                " gender: " + gender;
+    }
+}
---
+++ src/Mirea/TestAuthor.java
@@ -0,0 +1,8 @@
+package Mirea;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author aut = new Author("Se", "sE", 'm');
+        System.out.println(aut.toString());
+    }
+}
